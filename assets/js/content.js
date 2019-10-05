4b1df4d78f2218385d120c880fb2ea5fdf06115a
blackmorekwb <blackmorekwb@gmail.com>
2019-10-05 18:41:10 -0500

    Main UI 2.0 | Jquery UI dark theme

--- assets/js/content.js
+++ assets/js/content.js
@@ -11,14 +11,13 @@ for (var iVar = 0; iVar < buttons.length; iVar++) {
   try{throw iVar}
   catch(i) {
     buttons[i].onclick = function() {
-      //console.log("inside buttons each: i is -> " + i + "\n view is: #" + (i+1));
       content = $("#content" + (i+1))[0];
 
       if (content.className.includes(substr)) {  // shrink the box, if already opened
         content.style.backgroundColor = "#010201"; //black
         // Since div's are under bootstrap's Collapse, the "show" class is automatically toggled.
         //content.className = content.className.replace(" show", '');
-        //content.className = "main-content collapse";
+        clearMainContentExcept(i+0);
       } else {  // expand the box, if not already expanded
          content.style.backgroundColor = "#008000"; //green
          //content.className = content.className + " show";
@@ -27,12 +26,23 @@ for (var iVar = 0; iVar < buttons.length; iVar++) {
   } //catch
 } // for
 
-/*
+function clearMainContentExcept(window){
+//  for (var i = 0; i < contents.length; i++){  }
+  //debugger; // for in or with the i interval
 
-3()-----
-when one is clicked, hide the one currently being shown (by class name)
-replacing that window with the new content id
+  for ( var i = 0; i < contents.length; i++) {
+    //debugger; //first the repace line is bad. get rid of [0] - content is still set from above, not reset in this block
+      //then pull window param in and have it not reset that one.
+    content[0].className = content[0].className.replace(" show", '');
+    //debugger;
+  }
+  /*
+  for (content in contents) {
+    debugger;
+    content[0].className = content[0].className.replace(" show", '');
+    debugger;
+  }  */
+}
 
-
-
-*/
+// TODO : put this line in serperate JS file
+$( "#datepicker" ).datepicker({});
